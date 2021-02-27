4011862d28dc56275e816f31521f609f1d2e93d6
michalbez <77171995+michalbez@users.noreply.github.com>
2021-02-27 14:56:10 +0100

    Add files via upload
    Pierwsze porawki

--- js/func.js
+++ js/func.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+const output=document.getElementById("output")
+const button=document.getElementById("button")
+
+button.addEventListener('click',click)
+
+
+function click(){
+
+const input=document.getElementById("input").value
+output.innerHTML=input;
+
+
+}
